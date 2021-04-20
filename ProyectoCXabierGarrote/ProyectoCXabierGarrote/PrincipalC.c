fd94a5eacb6070f1663b5410c7035a70ab5ff4af
Xabier <xabier.100@hotmail.com>
2021-04-20 17:13:53 +0200

    Poniendo el menu principal a punto

--- ProyectoCXabierGarrote/ProyectoCXabierGarrote/PrincipalC.c
+++ ProyectoCXabierGarrote/ProyectoCXabierGarrote/PrincipalC.c
@@ -1,18 +1,44 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
-#define OPCIONES 5
+#include <Windows.h>
+#define TAM_OPCIONES 5
+void mostrarMenu();
+int pedirOpcion();
 main() {
-
-	void (*menuPuntero[OPCIONES - 1])() = {mostrarMenuClientes,mostrarMenu };
+	void (*menuPuntero[TAM_OPCIONES - 1])() = {mostrarMenuClientes,mostrarMenuServicios,presupuestos,facuracion};
+	
 	mostrarMenu();
 	int opc = pedirOpcion();
 	while (opc!=5)
 	{
-
-		menuPuntero[opc];
+		(*menuPuntero[opc-1])();
 		mostrarMenu();
 		opc = pedirOpcion();
 	}
-
-
 }
+
+void mostrarMenu()
+{
+	printf("MENU FACURACION\n");
+	printf("1-Clientes\n2-Servicios\n");
+	printf("3-Presupuestos\n4-Facuracion\n5-Salir");
+}
+int pedirOpcion() {
+	int opc;
+	printf("\nIntroduce Opcion: [ ]");
+	do
+	{
+		GotoXY(19, 6);
+		scanf("%d", &opc);
+	} while (opc < 1 || opc>6);
+	return opc;
+}
+void GotoXY(int x, int  y)
+{
+	COORD p;/*Cordenadas pa posicionar*/
+	p.X = x;
+	p.Y = y;
+	HANDLE h = GetStdHandle(STD_OUTPUT_HANDLE);/*Manejador del sistema en la consola*/
+	SetConsoleCursorPosition(h, p);
+}
+
